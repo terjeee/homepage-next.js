57282b081cda707304eb4eaa7cfa8b5861e15ad0
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-19 23:25:17 +0200

    -variable farger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      blue: "#0969da",
+      red: "#FF1B1C",
+      purple: "#DC6BAD",
+      orange: "#FFA400",
     },
   },
   plugins: [],
-}
+};
