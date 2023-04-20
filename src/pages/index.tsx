b189b7ad1c0fba52ba0f71298dcc29696c015e0b
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-20 05:52:20 +0200

    :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>HEI</h1>;
+  return <header className="mt-20">HEI</header>;
 }
