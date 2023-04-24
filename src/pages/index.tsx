5bc7a221c3427d7bf725e8646874f0a83fe7506d
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-24 03:12:41 +0200

    :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { Inter } from "next/font/google";
 
+import Typewriter from "@/components/sections/Typewriter";
+import SlideShow from "@/components/ui/SlideShow";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <header className="mt-20">HEI</header>;
+  return (
+    <main>
+      <Typewriter />
+      <SlideShow />
+    </main>
+  );
 }
