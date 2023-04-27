3ed7f172433f1643def45ed6010ba509d51622d7
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-27 04:33:51 +0200

    +activePath styling

--- src/components/layout/NavDesktop.tsx
+++ src/components/layout/NavDesktop.tsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import Login from "@/components/ui/Login";
 
 export default function NavDesktop() {
-  const urlPaths = [
+  const currentPath = useRouter().pathname;
+  const paths = [
     ["home", "/"],
     ["projects", "/"],
     ["contact", "/contact"],
   ];
 
+  console.log(currentPath);
+
   return (
-    <nav className="hidden py-4 sm:flex sm:justify-between">
+    <nav className="hidden py-4 sm:flex sm:justify-between items-center">
       <div className="flex gap-2">
-        {urlPaths.map(([title, url]) => (
-          <Link href={url} key={title} className="text-black font-medium hover:text-darkGrey ">
+        {paths.map(([title, path]) => (
+          <Link
+            href={path}
+            key={title}
+            className={`font-semibold ${
+              currentPath === path ? "text-black underline" : "text-darkGrey"
+            } hover:text-black underline-offset-8 hover:underline `}
+          >
             {title}
           </Link>
         ))}
