b5097d8ed691cb282f07eccff9cdd93510b44968
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-27 04:35:20 +0200

    :)

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -17,14 +17,12 @@ export default function Projects() {
           const colSpan = (index + 1) % 3 === 0 ? "col-span-2" : "col-span-1";
 
           return (
-            <li key={el.id} className={`p-3 ${colSpan} shadow-github rounded`}>
-              <div className="flex flex-col gap-2">
+            <li key={el.id} className={`${colSpan} shadow-github rounded cursor-pointer`}>
+              <a href={el.link} target="_blank" rel="noreferrer" className="p-3 flex flex-col gap-2">
                 <div className="flex justify-between">
                   <div className="flex justify-center items-center gap-2">
                     <IconRepository width="15px" />
-                    <a href={el.link} target="_blank" rel="noreferrer" className="hover:underline">
-                      <h2 className="text-sm font-bold text-link cursor-button">{el.title}</h2>
-                    </a>
+                    <h2 className="text-sm font-bold text-link cursor-button">{el.title}</h2>
                     <span className="py-[0.1rem] px-[0.25rem] mx-auto text-[9px] font-bold text-center border border-[#A9A9A9] rounded-full">
                       Public
                     </span>
@@ -38,7 +36,7 @@ export default function Projects() {
                   ></span>
                   <p className="text-[11px]">{el.text}</p>
                 </div>
-              </div>
+              </a>
             </li>
           );
         })}
