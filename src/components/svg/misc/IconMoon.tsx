b152391031a417016250a44ca674df3dfd6a3aaf
terjeee <96795863+terjeee@users.noreply.github.com>
2023-04-28 04:42:13 +0200

    +icon: sun, moon

---
+++ src/components/svg/misc/IconMoon.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from "react";
+
+export default function IconMoon(props: SVGProps<SVGSVGElement>) {
+  <svg xmlns="http://www.w3.org/2000/svg" width={800} height={800} fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      stroke="#323232"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M20.867 15.316c.052-.118-.066-.238-.188-.194v0c-3.362 1.215-7.23.498-9.767-2.038-2.525-2.525-3.243-6.367-2.05-9.718v0c.049-.139-.088-.273-.222-.213l-.027.012c-1.855.838-3.506 2.252-4.476 4.032a9.335 9.335 0 0 0 2.255 11.664A9.344 9.344 0 0 0 12.332 21a9.344 9.344 0 0 0 5.23-1.601c1.427-.964 2.557-2.423 3.27-4.003l.035-.08Z"
+    />
+  </svg>;
+}
