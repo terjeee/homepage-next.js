74b2592c5f236674c41e11cf93aa98c1ccbdf26e
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-04 06:17:51 +0200

    +contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import emailjs from "@emailjs/browser";
 
 import { regexEmail } from "@/utils/regex";
-import { env } from "process";
 
 export default function Contact() {
   const router = useRouter();
@@ -49,25 +48,24 @@ export default function Contact() {
   }
 
   return (
-    <div className="mx-4 h-full flex justify-center items-center gap-4">
-      {/* <h1 className="font-bold text-xl" style={{ writingMode: "vertical-rl", textOrientation: "upright" }}>
+    <div className="h-full flex justify-center items-center gap-4">
+      <h1 className="font-bold text-xl" style={{ writingMode: "vertical-rl", textOrientation: "upright" }}>
         CONTACT
-      </h1> */}
+      </h1>
       <form className="min-w-[45%] flex flex-col gap-3" onSubmit={sendEmail} ref={formData}>
         <div className="relative group">
           <input
             id="formEmail"
             type="email"
             name="formEmail"
+            placeholder="Email"
+            spellCheck={false}
             value={email}
             onChange={(event) => setEmail(event.target.value)}
-            className={`w-full p-2 text-[12px] border border-darkGrey text-black rounded
+            className={`p-2 w-full text-[11px] font-silkscreen text-black font-medium placeholder-black border border-darkGrey rounded
               ${emailValid ? "focus:outline-link" : "focus:outline-red"} ${emailValid ? "" : "border-2 border-red"}
             `}
           />
-          <label className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-2 font-silkscreen text-black text-[10px] group-focus-within:text-[10px] peer-valid:text-[10px] group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-full peer-valid:-translate-y-full group-focus-within:pl-0 peer-valid:pl-0">
-            Email
-          </label>
           {!emailValid && <p className="px-1 text-red text-[10px] font-semibold">Please enter a valid email.</p>}
         </div>
         <div>
@@ -75,16 +73,17 @@ export default function Contact() {
             name="formMsg"
             id="formMsg"
             placeholder="Message"
+            spellCheck={false}
             value={message}
             onChange={(event) => setMessage(event.target.value)}
-            className={`block p-2 w-full h-24 font-silkscreen text-[10px] text-black placeholder-black border border-darkGrey rounded ${
+            className={`block p-2 w-full h-24 text-[11px] font-silkscreen text-black font-medium placeholder-black border border-darkGrey rounded ${
               messageValid ? "focus:outline-link" : "focus:outline-red"
             } ${messageValid ? "" : "border-2 border-red"}`}
           />
           {!messageValid && <p className="px-1 text-red text-[10px] font-semibold">Message needs to be 5-250 characters.</p>}
         </div>
         <button
-          className="py-3 px-2 text-xs text-white font-silkscreen tracking-wider  bg-link border border-link rounded cursor-pointer disabled:bg-darkGrey disabled:border-darkGrey disabled:cursor-not-allowed"
+          className="py-3 px-2 text-xs text-white font-silkscreen font-bold tracking-wider bg-link border border-link rounded cursor-pointer disabled:bg-darkGrey disabled:border-darkGrey disabled:cursor-not-allowed"
           disabled={!formValid}
         >
           SEND
