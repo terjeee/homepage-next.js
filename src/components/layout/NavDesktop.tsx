3a67234bc2b50a588673919eea331e1cd616c976
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-07 20:53:53 +0200

    :)

--- src/components/layout/NavDesktop.tsx
+++ src/components/layout/NavDesktop.tsx
@@ -4,9 +4,10 @@ import { useRouter } from "next/router";
 
 import Login from "@/components/misc/Login";
 import SwitchDarkLight from "@/components/misc/SwitchDarkLight";
-import IconReact from "@/components//svg/technologies/IconReact";
-import IconSvelte from "../svg/technologies/IconSvelte";
-import IconVue from "../svg/technologies/IconVue";
+import IconReact from "@/components/svg/technologies/IconReact";
+import IconSvelte from "@/components/svg/technologies/IconSvelte";
+import IconVue from "@/components/svg/technologies/IconVue";
+import IconArrowDown from "@/components/svg/misc/IconArrowDown3";
 
 const paths = [
   ["home", "/"],
@@ -16,16 +17,16 @@ const paths = [
 
 export default function NavDesktop() {
   const currentPath = useRouter().pathname;
-  const [dropdownOpen, setDropdownOpen] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
     <nav className="hidden items-center py-4 sm:flex sm:justify-between">
-      <div className="flex gap-2">
+      <div className="flex gap-3">
         {paths.map(([title, path]) => (
           <Link
             href={path}
             key={title}
-            className={`font-semibold dark:text-white ${
+            className={`text-lg font-semibold dark:text-white ${
               currentPath !== path ? "" : "underline"
             } underline-offset-4 hover:underline dark:hover:text-white `}
           >
@@ -33,28 +34,29 @@ export default function NavDesktop() {
           </Link>
         ))}
         <div onMouseLeave={() => setDropdownOpen(false)} className="relative">
-          <button onMouseEnter={() => setDropdownOpen(true)} className="font-semibold">
+          <button onMouseEnter={() => setDropdownOpen(true)} className="flex items-center gap-1 text-lg font-semibold">
             sandbox
+            <IconArrowDown height={15} width={15} />
           </button>
           {dropdownOpen && (
-            <ul className="absolute flex flex-col rounded pt-2 shadow">
+            <ul className="absolute flex flex-col rounded py-1 shadow">
               <Link
                 href="/react"
-                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 text-sm hover:bg-lightGrey dark:bg-darkBg dark:text-white"
               >
                 <IconReact />
                 <h3>React</h3>
               </Link>
               <a
                 href="/"
-                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 text-sm hover:bg-lightGrey dark:bg-darkBg dark:text-white"
               >
                 <IconSvelte />
                 <h3>Svelte</h3>
               </a>
               <a
                 href="/"
-                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 text-sm hover:bg-lightGrey dark:bg-darkBg dark:text-white"
               >
                 <IconVue className="block" />
                 <h3>Vue</h3>
