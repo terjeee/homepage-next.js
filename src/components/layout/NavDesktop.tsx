59bf837cd30d8ee0cb6f34e15c439913e372b0ef
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-07 20:31:05 +0200

    + hover nav

--- src/components/layout/NavDesktop.tsx
+++ src/components/layout/NavDesktop.tsx
@@ -1,31 +1,67 @@
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 import Login from "@/components/misc/Login";
 import SwitchDarkLight from "@/components/misc/SwitchDarkLight";
+import IconReact from "@/components//svg/technologies/IconReact";
+import IconSvelte from "../svg/technologies/IconSvelte";
+import IconVue from "../svg/technologies/IconVue";
+
+const paths = [
+  ["home", "/"],
+  // ["projects", "/"],
+  ["contact", "/contact"],
+];
 
 export default function NavDesktop() {
   const currentPath = useRouter().pathname;
-  const paths = [
-    ["home", "/"],
-    // ["projects", "/"],
-    ["contact", "/contact"],
-  ];
+  const [dropdownOpen, setDropdownOpen] = useState(true);
 
   return (
-    <nav className="hidden py-4 sm:flex sm:justify-between items-center">
+    <nav className="hidden items-center py-4 sm:flex sm:justify-between">
       <div className="flex gap-2">
         {paths.map(([title, path]) => (
           <Link
             href={path}
             key={title}
-            className={`text-black dark:text-white font-semibold ${
-              currentPath === path ? "underline" : ""
-            } hover:text-black dark:hover:text-white underline-offset-4 hover:underline `}
+            className={`font-semibold dark:text-white ${
+              currentPath !== path ? "" : "underline"
+            } underline-offset-4 hover:underline dark:hover:text-white `}
           >
             {title}
           </Link>
         ))}
+        <div onMouseLeave={() => setDropdownOpen(false)} className="relative">
+          <button onMouseEnter={() => setDropdownOpen(true)} className="font-semibold">
+            sandbox
+          </button>
+          {dropdownOpen && (
+            <ul className="absolute flex flex-col rounded pt-2 shadow">
+              <Link
+                href="/react"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+              >
+                <IconReact />
+                <h3>React</h3>
+              </Link>
+              <a
+                href="/"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+              >
+                <IconSvelte />
+                <h3>Svelte</h3>
+              </a>
+              <a
+                href="/"
+                className="dark:hover:bg-gray-600 flex items-center gap-2 bg-white px-8 py-2 hover:bg-lightGrey dark:bg-darkBg dark:text-white"
+              >
+                <IconVue className="block" />
+                <h3>Vue</h3>
+              </a>
+            </ul>
+          )}
+        </div>
       </div>
       <div className="flex gap-2">
         <SwitchDarkLight />
