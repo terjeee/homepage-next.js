357a1de98b2dfdf34d2fe3854ee6daed00bc4264
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-07 20:53:47 +0200

    +icons

---
+++ src/components/svg/misc/IconArrowDown3.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from "react";
+
+export default function IconArrowDown2(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={800} height={800} fill="none" viewBox="0 0 24 24" {...props}>
+      <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 7v10m0 0 4-4m-4 4-4-4" />
+    </svg>
+  );
+}
