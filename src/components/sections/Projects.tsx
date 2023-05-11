099402b50afe7a0f00795fa02cb02a0471a66366
terjeee <96795863+terjeee@users.noreply.github.com>
2023-05-11 05:06:15 +0200

    :)

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -12,16 +12,16 @@ export default function Projects() {
     <MaxWidth width="max-w-screen-md">
       <h1 className="py-6 text-center font-silkscreen text-4xl tracking-widest">PROSJEKTER</h1>
       <SlideShow />
-      <ul className="my-6 flex flex-col gap-4 sm:grid sm:auto-cols-fr">
+      <ul className="my-6 flex flex-col gap-5 sm:grid sm:auto-cols-fr">
         {projects.map((el, index) => {
           const colSpan = (index + 1) % 3 === 0 ? "col-span-2" : "col-span-1";
           return (
             <li key={el.id} className={`${colSpan} cursor-pointer rounded shadow-github`}>
-              <a href={el.link} target="_blank" rel="noreferrer" className="flex flex-col gap-2 p-3">
+              <a href={el.link} target="_blank" rel="noreferrer" className="flex flex-col gap-2 p-4">
                 <div className="flex justify-between">
                   <div className="flex items-center justify-center gap-2">
                     <IconRepository width="15px" />
-                    <h2 className="cursor-button text-md font-bold text-link">{el.title}</h2>
+                    <h2 className="cursor-button text-md font-bold text-link md:text-lg">{el.title}</h2>
                     <span className="mx-auto rounded-full border border-[#A9A9A9] px-[0.25rem] py-[0.1rem] text-center text-[9px] font-bold">
                       Public
                     </span>
