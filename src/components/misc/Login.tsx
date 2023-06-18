eb120de0fb0e06ffe9e71b7fe9bae6df031fe556
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-19 01:01:43 +0200

    xd

--- src/components/misc/Login.tsx
+++ src/components/misc/Login.tsx
@@ -1,3 +1,7 @@
 export default function Login() {
-  return <button className="cursor-not-allowed rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white">LOGIN</button>;
+  return (
+    <button className="cursor-not-allowed rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white">
+      LOGIN
+    </button>
+  );
 }
