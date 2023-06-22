1c38ccad47a26b240e5fdd0574a401e700f43dc4
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-22 04:09:18 +0200

    footer alltid nederst på siden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className="dark:bg-darkBg dark:text-white">
-        <MaxWidth width="max-w-screen-md">
+        <MaxWidth width="max-w-screen-md" className="flex min-h-screen flex-col px-6">
           <Nav />
           {children}
           <Footer />
