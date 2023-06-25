8e0f0e908b5306e42808508b201b221d51c110bd
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-25 20:07:04 +0200

    :)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 
+const prismaClient = new PrismaClient();
+
 const handler = NextAuth({
+  adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  session: { strategy: "jwt" },
+  // pages: {
+  //   signIn: "/auth/signin",
+  //   signOut: "/auth/signout",
+  //   error: "/auth/error", // Error code passed in query string as ?error=
+  //   verifyRequest: "/auth/verify-request", // (used for check email message)
+  //   newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  // },
+  debug: false,
 });
 
 export { handler as GET, handler as POST };
