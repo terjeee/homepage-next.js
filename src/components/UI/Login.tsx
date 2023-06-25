deb1dbcff84b2bfa2a150550669e302c91d87e7a
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-25 20:09:41 +0200

    :)

--- src/components/UI/Login.tsx
+++ src/components/UI/Login.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Login() {
   return (
-    <button className="cursor-not-allowed rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white">
-      LOGIN
-    </button>
+    <Link
+      href="/api/auth/signin"
+      className=" cursor-pointer rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:scale-105 hover:bg-white hover:text-black"
+    >
+      SIGN IN
+    </Link>
   );
 }
+// TODO: legg til svart outline på hover
+// TODO: eventuelt border før/etter hover:
