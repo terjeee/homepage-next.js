9b54def1340ec7288c778b82df87eab4c69b82b1
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-27 13:57:05 +0200

    :)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,7 @@ const handler = NextAuth({
   //   verifyRequest: "/auth/verify-request", // (used for check email message)
   //   newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   // },
-  debug: false,
+  debug: true,
 });
 
 // TODO: provider -> credentials
