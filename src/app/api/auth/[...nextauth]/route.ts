34846277dc113e57e1e94feb9bd3b87a90b8d5f3
terjeee <96795863+terjeee@users.noreply.github.com>
2023-06-29 00:31:26 +0200

    clean up crew

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,7 @@ import GoogleProvider from "next-auth/providers/google";
 const prismaClient = new PrismaClient();
 
 const handler = NextAuth({
+  session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prismaClient),
   providers: [
@@ -14,15 +15,6 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: { strategy: "jwt" },
-  // pages: {
-  //   signIn: "/auth/signin",
-  //   signOut: "/auth/signout",
-  //   error: "/auth/error", // Error code passed in query string as ?error=
-  //   verifyRequest: "/auth/verify-request", // (used for check email message)
-  //   newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
-  // },
-  debug: false,
 });
 
 // TODO: provider -> credentials, github?
