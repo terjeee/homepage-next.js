a648c5931dd1524da05e097bba2fc98fe3c81f6c
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-01 02:29:03 +0200

    onClick={toggleMenu} på svelte/react-sandbox

--- src/components/layout/NavMobile.tsx
+++ src/components/layout/NavMobile.tsx
@@ -62,6 +62,7 @@ export default function NavMobile() {
                       initial={{ opacity: 0, translateY: -25 }}
                       animate={{ opacity: 1, translateY: 0 }}
                       transition={{ duration: 0.5, delay: 0.1 * 1 }}
+                      onClick={toggleMenu}
                       className="flex justify-center"
                     >
                       <a
@@ -77,6 +78,7 @@ export default function NavMobile() {
                       initial={{ opacity: 0, translateY: -25 }}
                       animate={{ opacity: 1, translateY: 0 }}
                       transition={{ duration: 0.5, delay: 0.1 * 2 }}
+                      onClick={toggleMenu}
                       className="flex justify-center"
                     >
                       <a
