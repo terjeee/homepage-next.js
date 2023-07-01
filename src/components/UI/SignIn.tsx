a6e5ca23b49d29a17d435acb50b750c68ef84427
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-01 16:43:06 +0200

    lagt til my-auto for å containe vertikal størrelse

--- src/components/UI/SignIn.tsx
+++ src/components/UI/SignIn.tsx
@@ -4,7 +4,7 @@ export default function SignIn() {
   return (
     <Link
       href="/api/auth/signin"
-      className=" cursor-pointer rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:scale-105 hover:bg-white hover:text-black dark:hover:bg-darkBg dark:hover:text-white"
+      className=" my-auto cursor-pointer rounded-full bg-orange px-4 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:scale-105 hover:bg-white hover:text-black dark:hover:bg-darkBg dark:hover:text-white"
     >
       SIGN IN
     </Link>
