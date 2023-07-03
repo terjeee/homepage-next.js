272edd13e4f24422984809d23cb0c18ebdf07820
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-03 14:12:17 +0200

    styling

--- src/components/layout/NavDesktop.tsx
+++ src/components/layout/NavDesktop.tsx
@@ -19,14 +19,14 @@ export default function NavDesktop() {
 
   return (
     <div className="hidden items-center py-4 sm:flex">
-      <ul className="flex gap-1">
+      <ul className="flex">
         {paths.map(([title, path]) => (
           <li key={title}>
             <Link
               href={path}
-              className={`rounded-md px-2 py-1 text-lg font-semibold dark:text-white ${
+              className={`rounded-md px-3 py-2 text-lg font-semibold dark:text-white ${
                 currentPath !== path ? "" : "underline"
-              } underline-offset-4 hover:bg-lightGrey dark:hover:text-white `}
+              } underline-offset-4 hover:bg-lightGrey dark:hover:bg-darkSecondary dark:hover:text-white `}
             >
               {title}
             </Link>
@@ -42,7 +42,7 @@ export default function NavDesktop() {
                 <a
                   href="https://svelte-sandbox-tau.vercel.app/"
                   target="_blank"
-                  className="flex items-center gap-2 bg-white px-8 py-2 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
+                  className="flex items-center gap-2 rounded-md bg-white px-8 py-3 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
                 >
                   <IconSvelte />
                   <h3>Svelte</h3>
@@ -52,7 +52,7 @@ export default function NavDesktop() {
                 <a
                   href="https://react-sandbox-kappa.vercel.app/"
                   target="_blank"
-                  className="flex items-center gap-2 bg-white px-8 py-2 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
+                  className="flex items-center gap-2 rounded-md bg-white px-8 py-3 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
                 >
                   <IconReact />
                   <h3>React</h3>
