c7f060c43eae401db6f0f1f1249d7dbcb293f924
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-15 03:22:44 +0200

    :)

--- src/components/layout/NavMobile.tsx
+++ src/components/layout/NavMobile.tsx
@@ -33,30 +33,43 @@ export default function NavMobile() {
         </div>
         {menuOpen && (
           <div className="z-40">
-            <motion.div
-              initial={{ translateX: -50 }}
-              animate={{ translateX: 0 }}
-              transition={{ duration: 0.2 }}
-              className="absolute inset-x-0 inset-y-0 h-screen bg-white dark:bg-darkBg"
-            >
-              <div className="flex h-full items-center justify-center">
-                <ul className="flex flex-col pb-28">
-                  <li className="py-3">
-                    <Link href="/" onClick={toggleMenu} className="flex items-center gap-2">
-                      <IconHome />
-                      <h2 className="font- text-lg font-semibold tracking-wider">home</h2>
+            <div className="absolute inset-x-0 inset-y-0 h-screen bg-white dark:bg-darkBg">
+              <div className="flex h-full items-center justify-end pr-6">
+                <ul className="flex flex-col gap-6">
+                  <motion.li
+                    initial={{ opacity: 0, translateY: -10 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.2 }}
+                    onClick={toggleMenu}
+                  >
+                    <Link href="/" onClick={toggleMenu} className="flex items-center justify-end gap-2">
+                      <h2 className="text-2xl font-semibold tracking-wider">home</h2>
+                      <IconHome height={37.5} width={37.5} />
                     </Link>
-                  </li>
-                  <li className="py-3">
-                    <Link href="/contact" onClick={toggleMenu} className="flex items-center gap-2">
-                      <IconMail />
-                      <h2 className="font- text-lg font-semibold tracking-wider">contact</h2>
+                  </motion.li>
+                  <motion.li
+                    initial={{ opacity: 0, translateY: -10 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.2, delay: 0.05 }}
+                    onClick={toggleMenu}
+                  >
+                    <Link href="/contact" onClick={toggleMenu} className="flex items-center justify-end gap-2">
+                      <h2 className="text-2xl font-semibold tracking-wider">contact</h2>
+                      <IconMail height={37.5} width={37.5} />
                     </Link>
-                  </li>
-                  <li className="relative py-3">
-                    <button onClick={() => toggleSandboxOpen((prevState) => !prevState)} className="flex items-center gap-2">
-                      <IconCode className="pt-[0.5px]" />
-                      <h2 className="font- text-lg font-semibold tracking-wider">sandbox</h2>
+                  </motion.li>
+                  <motion.li
+                    initial={{ opacity: 0, translateY: -10 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.2, delay: 0.1 }}
+                    className="relative"
+                  >
+                    <button
+                      onClick={() => toggleSandboxOpen((prevState) => !prevState)}
+                      className="flex items-center justify-end gap-2"
+                    >
+                      <h2 className="text-2xl font-semibold tracking-wider">sandbox</h2>
+                      <IconCode height={37.5} width={37.5} />
                     </button>
                     {sandboxOpen && (
                       <ul className="absolute w-full py-1">
@@ -70,7 +83,7 @@ export default function NavMobile() {
                           <a
                             href="https://svelte-sandbox-tau.vercel.app/"
                             target="_blank"
-                            className="flex items-center gap-1 bg-white py-2 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
+                            className="flex items-center gap-1 bg-white py-2 text-base font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
                           >
                             <IconSvelte />
                             <h3>Svelte</h3>
@@ -79,14 +92,14 @@ export default function NavMobile() {
                         <motion.li
                           initial={{ opacity: 0, translateY: -10 }}
                           animate={{ opacity: 1, translateY: 0 }}
-                          transition={{ duration: 0.2 }}
+                          transition={{ duration: 0.2, delay: 0.05 }}
                           onClick={toggleMenu}
                           className="flex justify-center"
                         >
                           <a
                             href="https://react-sandbox-kappa.vercel.app/"
                             target="_blank"
-                            className="flex items-center gap-1 bg-white py-2 text-sm font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
+                            className="flex items-center gap-1 bg-white py-2 text-base font-semibold hover:bg-lightGrey dark:bg-darkBg dark:text-white dark:hover:bg-darkSecondary"
                           >
                             <IconReact />
                             <h3>React</h3>
@@ -94,10 +107,10 @@ export default function NavMobile() {
                         </motion.li>
                       </ul>
                     )}
-                  </li>
+                  </motion.li>
                 </ul>
               </div>
-            </motion.div>
+            </div>
           </div>
         )}
       </div>
