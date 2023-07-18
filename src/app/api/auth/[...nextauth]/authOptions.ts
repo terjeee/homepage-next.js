57732c29ef8ce368887365b15ff49efa92cb0d27
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-18 23:39:47 +0200

    githubProvider + /signin

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -2,6 +2,7 @@ import { type AuthOptions } from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 const prismaClient = new PrismaClient();
 
@@ -11,10 +12,17 @@ const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
+  pages: {
+    signIn: "/signin",
+  },
 };
 
 // TODO: provider -> credentials, github?
--- src/components/UI/Authetication/SignIn.tsx
+++ src/components/UI/Authetication/SignIn.tsx
@@ -6,7 +6,7 @@ export default function SignIn() {
 
   return (
     <Link
-      href="/api/auth/signin"
+      href="/signin"
       className=" my-auto cursor-pointer rounded-full bg-orange px-3 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:rounded-full hover:bg-white hover:text-black dark:hover:bg-darkBg dark:hover:text-white sm:px-4"
     >
       SIGN IN
