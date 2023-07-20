00d05610ef6aeb52b0025d31022f513595f30dd3
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-20 23:23:01 +0200

    + user-dashboard

---
+++ src/app/(protected)/(user)/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashoard() {
+  return <div>Dashoard</div>;
+}
---
+++ src/app/(protected)/(user)/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import ArrowLeft from "@/components/svg/misc/ArrowLeft";
+import Authenticated from "@/components/UI/Authetication/Authenticated";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  if (status === "unauthenticated") return router.replace("/signin");
+
+  if (status === "authenticated") {
+    return (
+      <>
+        <nav className="group my-4 flex justify-between sm:my-8">
+          <button onClick={() => router.push("/")} className=" duration-100 hover:scale-[1.1] hover:cursor-pointer">
+            <ArrowLeft />
+          </button>
+          <Authenticated session={session} />
+        </nav>
+        {children}
+      </>
+    );
+  }
+}
