8826fc5f8fca220091f849b75b2f89ec27489740
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-21 22:40:46 +0200

    :)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
 import SVGMail from "@/components/svg/misc/SVGMail";
-import SVGGithub from "@/components/svg/misc/SVGGithub";
+// import SVGGithub from "@/components/svg/misc/SVGGithub";
 
 export default function Footer() {
   return (
     <footer className="mt-auto py-12">
       <div className="flex items-center justify-center gap-2">
         <a href="https://www.github.com/terjeee" target="_blank" rel="noreferrer" className="scale-90 hover:scale-105">
-          <SVGGithub />
+          {/* <SVGGithub /> */}
         </a>
         <Link href="/contact" className=" hover:scale-105">
           <SVGMail />
