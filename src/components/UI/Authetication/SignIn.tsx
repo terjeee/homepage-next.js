8c2c5f1bfc22e77611ee71310cecce52ed698704
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-29 22:31:46 +0200

    fikset border-radius onClick

--- src/components/UI/Authetication/SignIn.tsx
+++ src/components/UI/Authetication/SignIn.tsx
@@ -1,13 +1,10 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function SignIn() {
-  // const router = useRouter();
-
   return (
     <Link
       href="/signin"
-      className=" my-auto cursor-pointer rounded-full bg-orange px-3 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:rounded-full hover:bg-white hover:text-black dark:hover:bg-darkBg dark:hover:text-white sm:px-4"
+      className=" my-auto cursor-pointer rounded-full bg-orange px-3 py-2 text-xs font-medium tracking-widest text-white transition duration-200 hover:rounded-full hover:bg-white hover:text-black focus:rounded-full dark:hover:bg-darkBg dark:hover:text-white sm:px-4"
     >
       SIGN IN
     </Link>
