44e2053073f61d37500259238db76c51799e0726
terjeee <96795863+terjeee@users.noreply.github.com>
2023-07-29 22:34:37 +0200

    tregere animasjon på slideShow

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         github: "rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.2) 0px 0px 0px 1px",
       },
       animation: {
-        slideShow: "slideShow 50s linear infinite",
+        slideShow: "slideShow 70s linear infinite",
       },
       keyframes: {
         slideShow: {
