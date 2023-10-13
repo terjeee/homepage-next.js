e4ab38956272f52b77200c67b04ff2dbee0f811f
terjeee <96795863+terjeee@users.noreply.github.com>
2023-10-14 00:25:56 +0200

    added input fields og endret path etter log-in

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -24,7 +24,7 @@ export default function SignIn() {
     <section className="m-auto flex flex-col justify-center gap-4 p-4">
       <div className="flex items-center justify-center rounded-md border-[1px]">
         <button
-          onClick={() => signIn("google", { callbackUrl: "/" })}
+          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
           className="flex items-center justify-center gap-5 px-6 py-3"
         >
           <SVGGoogle />
@@ -33,13 +33,27 @@ export default function SignIn() {
       </div>
       <div className="flex items-center justify-center rounded-md border-[1px]">
         <button
-          onClick={() => signIn("github", { callbackUrl: "/" })}
+          onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
           className="flex items-center justify-center gap-5 px-6 py-3"
         >
           <SVGGithub />
           <span className="font-semibold">Continue with Github</span>
         </button>
       </div>
+      <hr className="my-2" />
+      <form action="submit" onSubmit={() => alert("Sign In")} className="flex flex-col gap-4">
+        {/* <label htmlFor="username">Username</label> */}
+        <input type="text" id="username" name="username" placeholder="username" className="rounded-md border px-2 py-1" />
+        {/* <label htmlFor="password">Password</label> */}
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          className="rounded-md border px-2 py-1"
+        />
+        <button type="submit">Sign In</button>
+      </form>
     </section>
   );
 }
