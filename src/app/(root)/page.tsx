8954e5e9a64197044fdfebafd2b63a54c6e2fabf
Terje <terjeovgg@gmail.com>
2023-10-27 17:32:30 +0200

    styling

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  router.push("/projects");
+  router.replace("/projects");
 
   return (
     <main>
--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -4,7 +4,7 @@ import ProjectList from "@/components/UI/ProjectList";
 export default function Projects() {
   return (
     <main>
-      <h1 className="my-12 pb-4 text-center font-silkscreen text-4xl tracking-[0.2em] md:mb-2 md:mt-20 md:pb-8">PROJECTS</h1>
+      <h1 className="md:mt-14s mb-12 mt-10 pb-4 text-center font-silkscreen text-4xl tracking-[0.2em] md:mb-8 md:pb-8">PROJECTS</h1>
       <SlideShow />
       <div className="my-4">
         <ProjectList />
