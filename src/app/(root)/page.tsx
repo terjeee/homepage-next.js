16285f8818ecc02d3d7c720a0e86265d91da60f3
Terje <terjeovgg@gmail.com>
2023-10-27 16:53:56 +0200

    routing, styling (margin)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 // import Header from "@/components/sections/Header";
 import Projects from "@/components/UI/ProjectList";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  router.push("https://watashi.no/projects");
+
   return (
     <main>
       {/* <Header /> */}
--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -4,7 +4,7 @@ import ProjectList from "@/components/UI/ProjectList";
 export default function Projects() {
   return (
     <main>
-      <h1 className="my-12 pb-4 text-center font-silkscreen text-4xl tracking-[0.2em] md:my-24 md:pb-8">PROJECTS</h1>
+      <h1 className="my-12 pb-4 text-center font-silkscreen text-4xl tracking-[0.2em] md:mb-2 md:mt-20 md:pb-8">PROJECTS</h1>
       <SlideShow />
       <div className="my-4">
         <ProjectList />
