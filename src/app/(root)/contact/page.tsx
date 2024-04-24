5e6f5838cb28f1c0803abfcf05e2f716b25ffe87
Terje <terjeovgg@gmail.com>
2024-04-24 19:42:14 +0200

    redesign: ContactPage

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,80 +1,114 @@
 "use client";
 
-import { useState, FormEvent, useRef } from "react";
+import { useState, useEffect, FormEvent, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+import { regexEmail } from "@/utils/regex";
+import SVGLoading from "@/components/svg/misc/SVGLoading";
 
 export default function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [emailValid, setEmailValid] = useState<boolean | undefined>(undefined);
+  const [messageValid, setMessageValid] = useState<boolean | undefined>(undefined);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const btnSubmit = useRef<HTMLButtonElement>(null);
-  const btnReset = useRef<HTMLButtonElement>(null);
+  const EMAILJS_KEY: undefined | string = process.env.EMAILJS_KEY;
+  const formData = useRef<HTMLFormElement>(null);
+  const formIsValid = email.length > 0 && regexEmail.test(email) && message.length >= 5 && message.length < 250;
 
-  function resetContactForm() {
-    setName("");
-    setEmail("");
-    setMessage("");
-  }
+  useEffect(() => {
+    setEmailValid(true);
 
-  function submitContactForm(event: FormEvent<HTMLFormElement>) {
+    const timeoutEmail = setTimeout(() => {
+      if (email.length > 0 && !regexEmail.test(email)) return setEmailValid(false);
+    }, 1000);
+    return () => clearTimeout(timeoutEmail); // return (useEffect) = clean-up før main code
+  }, [email]);
+
+  useEffect(() => {
+    setMessageValid(true);
+
+    const timeoutMessage = setTimeout(() => {
+      if (message.length !== 0 && (message.length < 5 || message.length > 100)) return setMessageValid(false);
+    }, 1000);
+    return () => clearTimeout(timeoutMessage); // return (+useEffect) = clean-up før main code
+  }, [message]);
+
+  async function handleSendEmail(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if (btnSubmit.current) btnSubmit.current.disabled = true;
+    if (formData.current) {
+      setIsSubmitting(true);
+      const response = await emailjs.sendForm("service_contactForm", "template_70fzocf", formData.current, EMAILJS_KEY);
 
-    // formValues === true
-    // TODO: POST form to email.js
-    // TODO: reset form values
-    // resetContactForm();
-    // TODO: enable buttons med useRef()
-    // TODO: loading på submitBTN
-    // TODO: Toast.tsx - success melding
+      if (response.text === "OK") {
+        // TODO: hotToast success
+        return;
+      } else {
+        return;
+        // TODO: hotToast error
+      }
 
-    // formValues === false
-    // TODO: error - Toast.tsx (egen komponent m/customization: bottom-left | bottom-middle | bottom-right )
+      setIsSubmitting(false);
 
-    if (btnSubmit.current) btnSubmit.current.disabled = false;
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
   }
 
   return (
-    <div className="my-24 flex flex-col items-center justify-center sm:my-40 sm:flex-row">
-      <form className="flex w-4/6 flex-col gap-3 sm:w-7/12" onSubmit={submitContactForm}>
+    <div className="my-20 flex flex-col items-center justify-center sm:my-32">
+      <form className="sm:w-/12 flex w-4/6 flex-col gap-3" onSubmit={handleSendEmail} ref={formData}>
         <input
+          id="formName"
+          name="formName"
           type="text"
-          placeholder="NAME"
           value={name}
           onChange={(event) => setName(event.target.value)}
-          className="placeholder-grey dark:placeholder-grey rounded-md border border-darkGrey px-2 py-3 font-silkscreen text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white"
-        />
-        <input
-          type="email"
-          placeholder="EMAIL"
+          placeholder="name"
           spellCheck={false}
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          className="placeholder-grey dark:placeholder-grey rounded-md border border-darkGrey px-2 py-3 font-silkscreen text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white sm:w-full"
+          className="placeholder-grey dark:placeholder-grey rounded-md border border-darkGrey px-2 py-3 font-quicksand text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white"
         />
-        <textarea
-          placeholder="Message.."
-          spellCheck={false}
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          className={`dark:placeholder-grey placeholder-grey block h-44 w-full resize-none rounded-md border border-darkGrey px-2 py-3 font-silkscreen text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white`}
-        />
-        <div className="flex gap-3">
-          <button
-            ref={btnSubmit}
-            className="text-md w-full cursor-pointer rounded-md border border-link bg-link px-2 py-3 font-silkscreen font-bold text-white focus:rounded-md dark:focus:border-white"
-          >
-            SEND
-          </button>
-          {/* <button
-            ref={btnReset}
-            onClick={resetContactForm}
-            className="text-md w-1/6 cursor-pointer rounded-md border border-darkGrey px-2 py-2 font-silkscreen font-bold text-white focus:rounded-md focus:border-white sm:py-2"
-          >
-            👻
-          </button> */}
-        </div>
+        <>
+          <input
+            id="formEmail"
+            name="formEmail"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="email"
+            spellCheck={false}
+            className="placeholder-grey dark:placeholder-grey w-full rounded-md border border-darkGrey px-2 py-3 font-quicksand text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white"
+          />
+          {emailValid === false && (
+            <p className="mb-1 text-[13px] font-bold text-red dark:font-semibold">Please enter a valid email</p>
+          )}
+        </>
+        <>
+          <textarea
+            id="formMsg"
+            name="formMsg"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            // placeholder="message"
+            spellCheck={false}
+            className={`dark:placeholder-grey placeholder-grey block h-36 w-full resize-none rounded-md border border-darkGrey px-2 py-3 font-quicksand text-sm font-medium text-black focus:rounded-md focus:border-black dark:bg-darkBg dark:text-white dark:focus:border-white`}
+          />
+          {messageValid === false && (
+            <p className="mb-1 pt-1 text-[13px] font-bold text-red dark:font-semibold">
+              Message needs to be 5-250 characters
+            </p>
+          )}
+        </>
+        <button
+          className="sm:text-md align-center flex w-full cursor-pointer justify-center rounded-md border border-link bg-link px-2 py-2 font-quicksand text-sm font-semibold tracking-wide text-white focus:rounded-md disabled:cursor-not-allowed disabled:border-darkGrey disabled:bg-darkGrey"
+          disabled={!formIsValid}
+        >
+          {isSubmitting ? <SVGLoading className="h-6" /> : "SEND MESSAGE"}
+        </button>
         {/* TODO: reCAPTCHA */}
       </form>
     </div>
